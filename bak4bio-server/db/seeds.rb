69146af80e66fe1ac1c6a9568c2d9419d7b66091
Anderson Andreatta <anderson@agivis.com.br>
2013-03-02 15:11:42 -0300

    Security

--- bak4bio-server/db/seeds.rb
+++ bak4bio-server/db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Cria o administrador do sistema
-admin_user = User.create({:name => 'Administrator', :email => "bak4bio@gmail.com", :password => "$bak4bio$"})
+admin_user = User.create({:name => 'Administrator', :email => "bak4bio@gmail.com", :password => "%=BAK4BIO=%"})
 Administrator.create({:user_id =>  admin_user.id})
